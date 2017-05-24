fab6bd1ca95b75af8cc3e2898907e66784adc9a3
Loi Tran <lvt15@my.fsu.edu>
2017-05-24 14:24:11 +0700

    added charles request

--- server.rb
+++ server.rb
@@ -17,16 +17,22 @@ end
 EasyTranslate.api_key= ENV['TRANSLATE_API_KEY']
 @@users = {}
 
-def language(id)
-  languages = EasyTranslate::LANGUAGES.invert
-  choice = @@users[id].downcase
-  languages.each do |key, value|
-    if key == choice
-      return {to: value.to_sym}
-    end
+# Original Way I wrote the method
+  # def language(id)
+  #   languages = EasyTranslate::LANGUAGES.invert
+  #   choice = @@users[id][:language].downcase
+  #   languages.each do |key, value|
+  #     if key == choice
+  #       return {to: value.to_sym}
+  #     end
+  #   end
+  #   return nil
+  # end
+
+# Cleaner way to write the language method
+  def language(id)
+    EasyTranslate::LANGUAGES.invert[@@users[id][:language].downcase]
   end
-  return nil
-end
 
 post '/callback' do
   data = JSON.parse(request.body.read)
@@ -37,26 +43,67 @@ post '/callback' do
       sender_id = messaging["sender"]["id"]
       text = messaging["message"]["text"]
 
-      unless @@users[sender_id]
-        Bot.new.send_message(sender_id, "To what language would you like me to translate? I know a lot")
-        Bot.new.send_message(sender_id, "Please respond with '/language'. For example, '/English'")
-      end
+      # unless @@users[sender_id]
+      #   Bot.new.send_message(sender_id, "To what language would you like me to translate? I know a lot")
+      #   Bot.new.send_message(sender_id, "Please respond with '/language'. For example, '/English'")
+      # end
+      #
+      # @@users[sender_id] ||= {}
+      # @@users[sender_id][:language] ||= "English"
+      # @@users[sender_id][:conversationType] ||= 'initial'
+      #
+      # if text[0] == "/"
+      #   @@users[sender_id][:language] = text[1..-1]
+      # end
+      #
+      # ap @@users
+      # choice = language(sender_id)
+      #
+      # if choice == nil
+      #   Bot.new.send_message(sender_id, "Sorry couldn't find #{@@users[sender_id][:language]} so I'm going to speak English for now.")
+      #   Bot.new.send_message(sender_id, "Please try another language with '/language'. For example, '/English' for translations to English")
+      # else
+      #   reply = EasyTranslate.translate(text, to: choice)
+      #   Bot.new.send_message(sender_id, reply)
+      # end
+      @@users[sender_id] ||= {}
+      @@users[sender_id][:language] ||= "English"
+      @@users[sender_id][:conversationType] ||= 'initial'
+      conversationType = @@users[sender_id][:conversationType]
+        case conversationType
+          when 'initial'
+            @@users[sender_id][:conversationType] = 'begun'
+            Bot.new.send_message(sender_id, "To what language would you like me to translate? I know a lot")
+            Bot.new.send_message(sender_id, "Please respond with '/language'. For example, '/English'")
+          when 'language'
+            @@users[sender_id][:language] = text
+            choice = language(sender_id)
+            if choice == nil
+              @@users[sender_id][:conversationType] ='begun'
+              Bot.new.send_message(sender_id, "Sorry couldn't find #{@@users[sender_id][:language]} so I'm going to speak English for now.")
+              Bot.new.send_message(sender_id, "Please try another language with '/language'. For example, '/English' for translations to English")
+            end
+            ap @@users
+            @@users[sender_id][:conversationType] = 'begun'
+            Bot.new.send_message(sender_id, "Ok, I'll translate to #{@@users[sender_id][:language]}")
+          when 'begun'
+            if text[0] == "/" && text != '/language'
+              @@users[sender_id][:language] = text[1..-1]
+              text = text[1..-1]
+            end
 
-      @@users[sender_id] ||= "English"
+            if text == "/language"
+              @@users[sender_id][:conversationType] = 'language'
+              Bot.new.send_message(sender_id, "What language would you like to select?")
+              text = text[1..-1]
+            end
 
-      if text[0] == "/"
-        @@users[sender_id] = text[1..-1]
-      end
-
-      choice = language(sender_id)
-
-      if choice == nil
-        Bot.new.send_message(sender_id, "Sorry couldn't find #{@@users[sender_id]} so I'm going to speak English for now.")
-        Bot.new.send_message(sender_id, "Please try another language with '/language'. For example, '/English' for translations to English")
-      else
-        reply = EasyTranslate.translate(text, choice)
-        Bot.new.send_message(sender_id, reply)
-      end
+            choice = language(sender_id)
+            reply = EasyTranslate.translate(text, to: choice)
+            ap @@users
+            Bot.new.send_message(sender_id, reply)
+          else
+        end
     end
   end
   'ok'
